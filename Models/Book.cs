70dc87c5ddc4060cc69ca0380c55d4bbd9dfabac
sajithapislk <sajith@apis.lk>
2024-11-15 13:13:37 +0530

    add models

---
+++ Models/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EBookStore.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EBookStore.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        // Navigation property
+        public virtual ICollection<Order> Orders { get; set; }
+    }
+}
