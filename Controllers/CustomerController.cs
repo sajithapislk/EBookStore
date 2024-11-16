d87416e991e5d1e626f3255dc529dcb0ec535bd0
sajithapislk <sajith@apis.lk>
2024-11-16 19:24:41 +0530

    login working

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.AspNet.Identity; // For using ASP.NET Identity's PasswordHasher
 
 namespace EBookStore.Controllers
 {
@@ -11,16 +14,18 @@ namespace EBookStore.Controllers
     {
         private ApplicationDbContext db = new ApplicationDbContext();
 
-        // GET: Customer
-        //public ActionResult Index()
-        //{
-        //    return View();
-        //}
         // GET: Customer/Register
         public ActionResult Register()
         {
             return View();
         }
+
+        // GET: Customer/Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
         // POST: Customer/Register
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -28,11 +33,41 @@ namespace EBookStore.Controllers
         {
             if (ModelState.IsValid)
             {
+                var passwordHasher = new PasswordHasher();
+                customer.Password = passwordHasher.HashPassword(customer.Password); // Use ASP.NET Identity's PasswordHasher
                 db.Customers.Add(customer);
                 db.SaveChanges();
                 return RedirectToAction("Login");
             }
             return View(customer);
+        }
+
+        // POST: Customer/Login
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string email, string password)
+        {
+            if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
+            {
+                ModelState.AddModelError("", "Email and Password are required.");
+                return View();
+            }
+
+            var customer = db.Customers.FirstOrDefault(c => c.Email == email);
+            if (customer != null)
+            {
+                var passwordHasher = new PasswordHasher();
+                var verificationResult = passwordHasher.VerifyHashedPassword(customer.Password, password);
+
+                if (verificationResult == PasswordVerificationResult.Success)
+                {
+                    Session["CustomerId"] = customer.CustomerId;
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+
+            ModelState.AddModelError("", "Invalid email or password.");
+            return View();
         }
     }
 }
--- Models/Customer.cs
+++ Models/Customer.cs
@@ -7,18 +7,23 @@ namespace EBookStore.Models
     public class Customer
     {
         public int CustomerId { get; set; }
+
         [Required(ErrorMessage = "First Name is required.")]
+        [StringLength(50, ErrorMessage = "First Name can't be longer than 50 characters.")]
         public string FirstName { get; set; }
 
         [Required(ErrorMessage = "Last Name is required.")]
+        [StringLength(50, ErrorMessage = "Last Name can't be longer than 50 characters.")]
         public string LastName { get; set; }
 
         [Required(ErrorMessage = "Email is required.")]
         [EmailAddress(ErrorMessage = "Invalid email format.")]
+        [StringLength(100, ErrorMessage = "Email can't be longer than 100 characters.")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "Password is required.")]
-        [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters long.")]
+        [StringLength(255, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters long.")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
 
         // Navigation property
