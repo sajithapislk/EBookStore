153c295ac1b5f1ad1165ac5fc833fd0574ed09bb
sajithapislk <sajith@apis.lk>
2024-12-17 12:16:18 +0530

    book search working

---
+++ Controllers/BookController.cs
@@ -0,0 +1,23 @@
+﻿using EBookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EBookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Search
+        public ActionResult Search(string query)
+        {
+            var books = db.Books
+                .Where(b => b.Title.Contains(query) || b.Author.Contains(query) || b.Description.Contains(query))
+                .ToList();
+            return View(books);
+        }
+    }
+}
