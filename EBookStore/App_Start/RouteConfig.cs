10fc4a0e5cb440c25a11e0c128b293af81314bd7
sajithapislk <sajith@apis.lk>
2024-12-20 21:18:39 +0530

    Author working

--- EBookStore/App_Start/RouteConfig.cs
+++ EBookStore/App_Start/RouteConfig.cs
@@ -13,6 +13,9 @@ namespace EBookStore
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            // Enable attribute routing
+            routes.MapMvcAttributeRoutes();
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
---
+++ EBookStore/Controllers/Admin/AuthorController.cs
@@ -0,0 +1,98 @@
+﻿using EBookStore.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace EBookStore.Controllers.Admin
+{
+    [RoutePrefix("Admin/Author")]
+    public class AuthorController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Admin/Author
+        public ActionResult Index()
+        {
+            var authors = db.Authors.ToList();
+            return View(authors);
+        }
+
+        // GET: Admin/Author/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Author/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Author author)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Authors.Add(author);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(author);
+        }
+
+        // GET: Admin/Author/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            var author = db.Authors.Find(id);
+            if (author == null)
+            {
+                return HttpNotFound();
+            }
+            return View(author);
+        }
+
+        // POST: Admin/Author/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Author author)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(author).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(author);
+        }
+
+        // GET: Admin/Author/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            var author = db.Authors.Find(id);
+            if (author == null)
+            {
+                return HttpNotFound();
+            }
+            return View(author);
+        }
+
+        // POST: Admin/Author/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var author = db.Authors.Find(id);
+            if (author != null)
+            {
+                db.Authors.Remove(author);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+    }
+}
